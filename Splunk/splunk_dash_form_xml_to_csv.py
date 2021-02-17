c2064132cb877167db485c8c6af4af3f0f1155c1
Ammar Ali <ammar@sumologic.com>
2021-02-17 11:57:52 +0000

    adding simple query translation

--- Splunk/splunk_dash_form_xml_to_csv.py
+++ Splunk/splunk_dash_form_xml_to_csv.py
@@ -75,9 +75,7 @@ def main():
               query = str(query)
               splunk_query = re.sub(r"\s*\|\s*", "\n| ", query)
               sumo_query = rex_to_parse_regex(splunk_query)
-              sumo_query = replace_headers(sumo_query, 'index=$environment$ host="mue*" source="/opt/notification/logs/stats.log"', '_sourcecategory=npe/*/ncs/spoc/stats_log _sourceName=/opt/*/logs/stats.log _sourceHost=mu*')
-              sumo_query = replace_headers(sumo_query, 'index=$environment$ host="mue*" source="/opt/spoc/logs/stats.log"', '_sourcecategory=npe/*/ncs/spoc/stats_log _sourceName=/opt/*/logs/stats.log _sourceHost=mu*')
- 
+        
            
               row['Source File'] = src_file
               row['Dashboard Name'] = dash_name
